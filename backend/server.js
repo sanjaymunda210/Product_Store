0e9f663d1fddce00ea9d7ef96ff06887b0da7501
Sanjay <sanjaymunda0123@gmail.com>
2025-08-23 16:42:45 +0530

    Api Integeration

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("server is ready");
-});
+app.use(express.json()); // allow us to accept JSON data in the req.body
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
